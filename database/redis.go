6f1ad0283103f580178d7684282d77c922248424
Marcon Neves <marcon@neves.run>
2024-11-04 05:54:58 -0300

    feat: add redis create integration

--- database/redis.go
+++ database/redis.go
@@ -2,10 +2,10 @@ package database
 
 import "github.com/marconneves/coolify-sdk-go/client"
 
-type CreateDatabasePostgresDTO struct {
+type CreateDatabaseRedisDTO struct {
 	ServerUUID      string  `json:"server_uuid"`
 	ProjectUUID     string  `json:"project_uuid"`
-	Environment     string  `json:"environment_name"`
+	EnvironmentName string  `json:"environment_name"`
 	Name            *string `json:"name,omitempty"`
 	Description     *string `json:"description,omitempty"`
 	Image           *string `json:"image,omitempty"`
@@ -14,12 +14,8 @@ type CreateDatabasePostgresDTO struct {
 	InstantDeploy   *bool   `json:"instant_deploy,omitempty"`
 	DestinationUUID *string `json:"destination_uuid,omitempty"`
 
-	PostgresUser           *string `json:"postgres_user,omitempty"`
-	PostgresPassword       *string `json:"postgres_password,omitempty"`
-	PostgresDB             *string `json:"postgres_db,omitempty"`
-	PostgresInitdbArgs     *string `json:"postgres_initdb_args,omitempty"`
-	PostgresHostAuthMethod *string `json:"postgres_host_auth_method,omitempty"`
-	PostgresConf           *string `json:"postgres_conf,omitempty"`
+	RedisPassword *string `json:"redis_password,omitempty"`
+	RedisConf     *string `json:"redis_conf,omitempty"`
 
 	LimitsMemory            *string `json:"limits_memory,omitempty"`
 	LimitsMemorySwap        *string `json:"limits_memory_swap,omitempty"`
@@ -30,22 +26,22 @@ type CreateDatabasePostgresDTO struct {
 	LimitsCPUShares         *int    `json:"limits_cpu_shares,omitempty"`
 }
 
-type CreateDatabasePostgresResponse struct {
+type CreateDatabaseRedisResponse struct {
 	UUID string `json:"uuid"`
 }
 
-func (d *DatabaseInstance) CreatePostgreSQL(data *CreateDatabasePostgresDTO) (*string, error) {
+func (d *DatabaseInstance) CreateRedis(data *CreateDatabaseRedisDTO) (*string, error) {
 	buf, err := client.EncodeRequest(data)
 	if err != nil {
 		return nil, err
 	}
 
-	body, err := d.client.HttpRequest("databases/postgresql", "POST", *buf)
+	body, err := d.client.HttpRequest("databases/redis", "POST", *buf)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := client.DecodeResponse(body, &CreateDatabasePostgresResponse{})
+	response, err := client.DecodeResponse(body, &CreateDatabaseRedisResponse{})
 	if err != nil {
 		return nil, err
 	}
